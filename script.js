30177e84bc8777a69e9c75987b66c77921e36559
yoroih <roycristhopherqp@gmail.com>
2024-05-21 18:24:15 -0500

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+const d = document;
+const $table = d.querySelector(".crud-table");
+const $form = d.querySelector(".crud-from");
+const $title = d.querySelector(".crud-title");
+const $template = d.getElementById("crud-template").content;
+const $fragment = d.createDocumentFragment();
+
+console.log("add");
